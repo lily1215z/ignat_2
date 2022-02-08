c4b9e75952cceda80f4f2cd7a9087de79eec9978
lily1215z <odessa1215@i.ua>
2022-02-09 00:00:07 +0200

    added some differents

--- src/p2-homeworks/h5/HeaderMenu.tsx
+++ src/p2-homeworks/h5/HeaderMenu.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import {NavLink} from 'react-router-dom'
 import s from './HeaderNav.module.css'
+import {PATH} from './Routing'
 
 type setActiveType = ((props: { isActive: boolean }) => string)
 
@@ -8,16 +9,13 @@ const HeaderMenu = () => {
     const setActive: setActiveType = ({isActive}) => isActive ? `${s.active}` : s.link
     const [menuActive, setMenuActive] = useState(false)
 
-    //закоментила в стилях св-во скрытия
     return (
         <>
             <div className={s.push} onClick={() => setMenuActive(!menuActive)}>PUSH</div>
-            {menuActive && <div className={menuActive ? `${s.nav_block} ${s.active_nav}` : `${s.nav} ${s.nav_block}`}>
-                <div>
-                    <NavLink to={'/pre-junior'} className={setActive}>prev-junior</NavLink>
-                    <NavLink to={'/junior'} className={setActive}>junior</NavLink>
-                    <NavLink to={'/junior-plus'} className={setActive}>junior-plus</NavLink>
-                </div>
+            {menuActive && <div className={s.nav}>
+                    <NavLink to={PATH.PRE_JUNIOR} className={setActive}>prev-junior</NavLink>
+                    <NavLink to={PATH.JUNIOR} className={setActive}>junior</NavLink>
+                    <NavLink to={PATH.JUNIOR_PLUS} className={setActive}>junior-plus</NavLink>
             </div>}
         </>
     )
