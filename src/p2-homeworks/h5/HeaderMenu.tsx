ee3aa6ed8c3a312f9b16f8705d11485663586c8a
lily1215z <odessa1215@i.ua>
2022-02-08 13:18:01 +0200

    fixed

--- src/p2-homeworks/h5/HeaderMenu.tsx
+++ src/p2-homeworks/h5/HeaderMenu.tsx
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
-import { NavLink } from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 import s from './HeaderNav.module.css'
 
-type setActiveType = ((props: {isActive: boolean}) => string)
+type setActiveType = ((props: { isActive: boolean }) => string)
 
 const HeaderMenu = () => {
     const setActive: setActiveType = ({isActive}) => isActive ? `${s.active}` : s.link
@@ -11,12 +11,14 @@ const HeaderMenu = () => {
     //закоментила в стилях св-во скрытия
     return (
         <>
-            <div className={s.push} onClick={()=> setMenuActive(!menuActive)}>PUSH</div>
-            <div className={menuActive ? `${s.nav_block} ${s.active_nav}` : `${s.nav} ${s.nav_block}`}>
-                <NavLink to={'/pre-junior'} className={setActive}>prev-junior</NavLink>
-                <NavLink to={'/junior'} className={setActive}>junior</NavLink>
-                <NavLink to={'/junior-plus'} className={setActive}>junior-plus</NavLink>
-            </div>
+            <div className={s.push} onClick={() => setMenuActive(!menuActive)}>PUSH</div>
+            {menuActive && <div className={menuActive ? `${s.nav_block} ${s.active_nav}` : `${s.nav} ${s.nav_block}`}>
+                <div>
+                    <NavLink to={'/pre-junior'} className={setActive}>prev-junior</NavLink>
+                    <NavLink to={'/junior'} className={setActive}>junior</NavLink>
+                    <NavLink to={'/junior-plus'} className={setActive}>junior-plus</NavLink>
+                </div>
+            </div>}
         </>
     )
 }
